3f5bf05ba385c199d8abcd58a7224e09f70a6d32
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-01 15:43:13 +0800

    test ci

--- .github/actions/CI/index.ts
+++ .github/actions/CI/index.ts
@@ -1,1 +1,13 @@
-console.log("CI Pass")
+import { readFileSync } from "fs";
+
+console.log(import.meta.dirname);
+console.log(process.argv);
+
+try {
+  console.log(readFileSync("./gia_gen/test.ts", "utf-8"));
+} catch (e) {
+  console.log(e);
+}
+
+
+console.log("CI Pass");
