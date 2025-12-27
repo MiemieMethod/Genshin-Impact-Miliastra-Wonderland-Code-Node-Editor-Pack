b87fde692b25188181a4a6a9ab9dc363fa4b4897
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-27 17:42:28 +0800

    update

--- .github/workflows/scripts/translate.ts
+++ .github/workflows/scripts/translate.ts
@@ -3,9 +3,7 @@ import fg from "fast-glob";
 import fse from "fs-extra";
 import { GoogleGenAI } from "@google/genai";
 
-import * as core from '@actions/core';
-
-const apiKey = core.getInput('API_KEY');
+const apiKey = process.env.GEMINI_API_KEY;
 if (!apiKey) {
   console.error("Error: GEMINI_API_KEY is not set in environment variables.");
   process.exit(1);
