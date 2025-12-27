5b62ec059ea44f2d82997dd04d812cbc92e9a8d5
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-27 09:54:25 +0800

    update protobuf

--- temp.cs
+++ temp.cs
@@ -2,45 +2,101 @@ graph {
   id {
     class: Node
     type: ClientGraph
-    id: 1082130433
+    id: 1082130437
   }
-  name: "New Skill Node Graph"
-  which: Skills
+  name: "New Filter Node Graph_2"
+  which: IntegerFilter
   graph {
     inner {
       graph {
         id {
           class: UserDefined
-          type: Skills
+          type: IntegerFilter
           kind: NodeGraph
-          id: 1082130433
+          id: 1082130437
         }
-        name: "New Skill Node Graph"
+        name: "New Filter Node Graph_2"
+                nodes {
+          nodeIndex: 3
+          genericId {
+            class: SystemDefined
+            type: Filter
+            kind: SysCall
+            nodeId: 200122
+          }
+          concreteId {
+            class: SystemDefined
+            type: Filter
+            kind: SysCall
+          }
+          pins {
+            i1 {
+              kind: InParam
+            }
+            i2 {
+              kind: InParam
+            }
+            value {
+              class: IntBase
+              itemType {
+                classBase: Client
+                type_client {
+                  type: Integer_
+                }
+              }
+              bInt {
+              }
+            }
+            type: 3
+            connects {
+              id: 1
+              connect {
+                kind: OutParam
+              }
+              connect2 {
+                kind: OutParam
+              }
+            }
+          }
+          pins {
+            i1 {
+              kind: InParam
+              index: 1
+            }
+            i2 {
+              kind: InParam
+              index: 1
+            }
+            value {
+              class: EnumBase
+              itemType {
+                classBase: Client
+                type_client {
+                  type: EnumItem_
+                }
+              }
+              bEnum {
+                val: 10000011
+              }
+            }
+            type: 13
+          }
+          x: 85.6
+          y: -156
+        }
         nodes {
           nodeIndex: 1
           genericId {
             class: SystemDefined
-            type: Skill
+            type: Filter
             kind: SysCall
-            nodeId: 200042
+            nodeId: 200011
           }
           concreteId {
             class: SystemDefined
-            type: Skill
+            type: Filter
             kind: SysCall
-            nodeId: 2001
-          }
-          8 {
-            1: 6
-          }
-        }
-        nodes {
-          nodeIndex: 2
-          genericId {
-            class: SystemDefined
-            type: Skill
-            kind: SysCall
-            nodeId: 200005
+            nodeId: 30
           }
           pins {
             i1 {
@@ -50,69 +106,15 @@ graph {
               kind: InParam
             }
             value {
-              class: ConcreteBase
-              bConcreteValue {
-                indexOfConcrete: -1
-              }
-            }
-          }
-          pins {
-            i1 {
-              kind: InParam
-              index: 1
-            }
-            i2 {
-              kind: InParam
-              index: 1
-            }
-            value {
-              class: ConcreteBase
-              bConcreteValue {
-                indexOfConcrete: -1
-              }
-            }
-          }
-          x: -285.6
-          y: -411.4
-        }
-        nodes {
-          nodeIndex: 3
-          genericId {
-            class: SystemDefined
-            type: Skill
-            kind: SysCall
-            nodeId: 200005
-          }
-          concreteId {
-            class: SystemDefined
-            type: Skill
-            kind: SysCall
-            nodeId: 10
-          }
-          pins {
-            i1 {
-              kind: InParam
-            }
-            i2 {
-              kind: InParam
-            }
-            value {
-              class: ConcreteBase
-              alreadySetVal: true
-              bConcreteValue {
-                value {
-                  class: EnumBase
-                  alreadySetVal: true
-                  itemType {
-                    classBase: Client
-                    type_client {
-                      type: EnumItem_
-                    }
-                  }
-                  bEnum {
-                    val: 102
-                  }
+              class: EnumBase
+              itemType {
+                classBase: Client
+                type_client {
+                  type: EnumItem_
                 }
+              }
+              bEnum {
+                val: 300
               }
             }
             type: 13
@@ -131,106 +133,84 @@ graph {
               alreadySetVal: true
               bConcreteValue {
                 value {
-                  class: EnumBase
+                  class: IntBase
                   alreadySetVal: true
                   itemType {
                     classBase: Client
                     type_client {
-                      type: EnumItem_
+                      type: Integer_
                     }
                   }
-                  bEnum {
-                    val: 104
+                  bInt {
+                    val: 1
                   }
                 }
               }
             }
-            type: 13
-          }
-          x: -29.6
-          y: -341
-        }
-        nodes {
-          nodeIndex: 4
-          genericId {
-            class: SystemDefined
-            type: Skill
-            kind: SysCall
-            nodeId: 200005
-          }
-          concreteId {
-            class: SystemDefined
-            type: Skill
-            kind: SysCall
-            nodeId: 10
+            type: 3
           }
           pins {
             i1 {
               kind: InParam
+              index: 2
             }
             i2 {
               kind: InParam
+              index: 2
             }
             value {
               class: ConcreteBase
               alreadySetVal: true
               bConcreteValue {
-                indexOfConcrete: 4
                 value {
-                  class: EnumBase
-                  alreadySetVal: true
+                  class: IntBase
                   itemType {
                     classBase: Client
                     type_client {
-                      type: EnumItem_
+                      type: Integer_
                     }
                   }
-                  bEnum {
-                    val: 500
+                  bInt {
                   }
                 }
               }
             }
-            type: 13
+            type: 3
           }
           pins {
             i1 {
-              kind: InParam
-              index: 1
+              kind: OutParam
             }
             i2 {
-              kind: InParam
-              index: 1
+              kind: OutParam
             }
             value {
               class: ConcreteBase
               alreadySetVal: true
               bConcreteValue {
-                indexOfConcrete: 4
                 value {
-                  class: EnumBase
-                  alreadySetVal: true
+                  class: IntBase
                   itemType {
                     classBase: Client
                     type_client {
-                      type: EnumItem_
+                      type: Integer_
                     }
                   }
-                  bEnum {
-                    val: 501
+                  bInt {
                   }
                 }
               }
             }
-            type: 13
+            type: 3
           }
-          x: 460
-          y: -340.2
+          x: -578.4
+          y: -5
         }
-        xxx: 1
+        ioNodeIndex: 3
+        xxxx: 0.3
       }
     }
   }
 }
-filePath: "201176311-1766797686-1073741840-\\1.gia"
+filePath: "201176311-1766800208-1073741840-\\_2.gia"
 gameVersion: "6.2.0"
--- utils/protobuf/struct.test.ts
+++ utils/protobuf/struct.test.ts
@@ -4,8 +4,11 @@ import { decode_gia_file, encode_gia_file } from "./decode.ts";
 
 const PATH = "C:/Users/admin/AppData/LocalLow/miHoYo/原神/BeyondLocal/Beyond_Local_Export/";
 
-const n = decode_gia_file(PATH + "temp.gia");
-n.graph.graph!.inner.graph.nodes[0].pins[0].value.bConcreteValue!.value = {} as any;
-n.graph.graph!.inner.graph.nodes[1].pins[0].value.bConcreteValue!.value = {} as any;
-n.graph.graph!.inner.graph.affiliations = [];
-encode_gia_file(PATH + "temp2.gia", n);
+const n = decode_gia_file(PATH + "2.gia");
+n.graph.graph!.inner.graph.nodes[0].nodeIndex = 3;
+n.graph.graph!.inner.graph.nodes[0].pins[0].connects[0].id = 1;
+n.graph.graph!.inner.graph.ioNodeIndex = 3;
+n.graph.graph!.inner.graph.nodes[1].nodeIndex = 1;
+// n.graph.graph!.inner.graph.nodes[1].pins[0].value.bConcreteValue!.value = {} as any;
+// n.graph.graph!.inner.graph.affiliations = [];
+encode_gia_file(PATH + "2_.gia", n);
