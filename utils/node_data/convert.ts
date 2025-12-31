48c125b977b12f6ddb56d9ec5565d48a2ca88660
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-31 18:54:03 +0800

    change function name string to Action. Now NodeDef.Identifier is all complete

--- utils/node_data/convert.ts
+++ utils/node_data/convert.ts
@@ -2,19 +2,24 @@ import { readFileSync, writeFileSync } from "fs";
 import data from "./data.json" with {type: "json"};
 import { assert, assertDeepEq, assertEq, exclude_keys } from "../utils.ts";
 const read = (path: string) => readFileSync(import.meta.dirname + "/" + path).toString();
-const save = (path: string, data: {}) => writeFileSync(import.meta.dirname + "/" + path, JSON.stringify(data, null, 2));
+const save = (path: string, data: {} | string) => writeFileSync(import.meta.dirname + "/" + path, typeof data === "string" ? data : JSON.stringify(data, null, 2));
 
+// const names = data.Nodes.map(x => x.Identifier).sort();
+// assertEq(names.length, new Set(names).size);
 
-let a = data.Nodes.filter(x => x.System === "Server").sort((l, r) => l.Identifier.localeCompare(r.Identifier)).map(x => x.Identifier.split(".").slice(0, 2).join("."));
-let b = data.Nodes.filter(x => x.System === "Client").sort((l, r) => l.Identifier.localeCompare(r.Identifier)).map(x => x.Identifier.split(".").slice(0, 2).join(".") + " Client");
+// save("temp.txt", names.join("\n"));
 
-const raw = [...new Set([...a, ...b].sort())];
-const res = new Map(read("temp.txt").split("\n").map((x, i) => (assertEq(x.split(":")[0].trim(), raw[i]), x.split(":").map(x => x.trim()))));
+const ref = new Map(read("temp.txt").split("\n").map(x => x.split(":").map(y => y.trim())));
+// const names = data.Nodes.map(x => x.Identifier).sort();
+// names.forEach((x, i) => assertEq(x, ref[i].split(":")[0]));
 
 data.Nodes.forEach(x => {
-  let id = x.Identifier.split(".");
-  id[1] = res.get(id[0] + "." + id[1] + (x.System === "Client" ? " Client" : ""))!.trim();
-  x.Identifier = id.join(".");
+  const d = x.Identifier.split(".");
+  d[2] = ref.get(x.Identifier)!;
+  assert(d[2].length > 0);
+  x.Identifier = d.join(".");
 });
+
+assert(data.Nodes.every(x => !x.Identifier.includes("undefined")));
 
 save("data.json", data);
