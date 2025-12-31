a5f0526400faf0f5359c8dfbf5f5ea2b65ea293a
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-31 20:39:27 +0800

    1

--- utils/node_data/convert.ts
+++ utils/node_data/convert.ts
@@ -2,40 +2,54 @@ import { readFileSync, writeFileSync } from "fs";
 import data from "./data.json" with {type: "json"};
 import { assert, assertDeepEq, assertEq, exclude_keys } from "../utils.ts";
 import { nodeDefinitions as REF } from "../../ref/Columbina-Dev/WebMiliastraNodesEditor/src/data/nodeDefinitions.ts"
+import { parse, stringify } from "yaml";
 const read = (path: string) => readFileSync(import.meta.dirname + "/" + path).toString();
 const save = (path: string, data: {} | string) => writeFileSync(import.meta.dirname + "/" + path, typeof data === "string" ? data : JSON.stringify(data, null, 2));
 
-// console.log(data.Nodes.map(x => x.Identifier.length));
+// save("enum_lookup.yaml", stringify({
+//   Server: data.Enums.filter(x => x.System === "Server").map(x => Object.fromEntries([
+//     [x.Identifier, x.ID],
+//     ["name", x.InGameName.en],
+//     ["items", Object.fromEntries(x.Items.map(y => [y.Identifier, y.ID]))]
+//   ])),
+//   Client: data.Enums.filter(x => x.System === "Client").map(x => Object.fromEntries([
+//     [x.Identifier, x.ID],
+//     ["name", x.InGameName.en],
+//     ["items", Object.fromEntries(x.Items.map(y => [y.Identifier, y.ID]))]
+//   ]))
+// }));
 
-// save("temp.txt", data.Nodes.map(x => x.Identifier).sort((a, b) => -a.length + b.length).join("\n"))
+// assertEq(new Set(data.Enums.map(x => x.Identifier)).size, data.Enums.length);
 
-const CREF = REF.filter(x => x.id.startsWith("client."))
-  .map(x => ({ ...x, _name: x.displayNameEN.replace(/[^A-Za-z]/g, "").toUpperCase() }));
+// 修改全部枚举类型为标准枚举
+data.Nodes.forEach(node => {
+  node.DataPins.forEach(p => {
+    let q = /^E\<.+\>$/.exec(p.Type);
+    if (q) {
+      p.
+    }
+  })
+})
 
 data.Nodes.forEach(node => {
-  let ref = REF.filter(x => x.officialID === node.ID && !x.id.startsWith("client"));
-  assert(ref.length <= 1);
-  if (ref.length === 0) {
-    if (node.System !== "Client") {
-      console.log("Not Matched", node.Identifier);
-      node.__not_find_in_ref = true;
-      return;
-    }
-    const pat = node.InGameName.en.replace(/[^A-Za-z]/g, "").toUpperCase();
-    ref = CREF.filter(x => x._name === pat);
-    if (ref.length !== 1) {
-      console.log("Not Matched", node.Identifier);
-      node.__not_find_in_ref = true;
-      return;
-    } else {
-      // console.log(node.InGameName.en, p[0].displayNameEN);
-    }
+  // 确实有正确的引脚
+  assert(node.DataPins.filter(x => x.Direction === "In").every((x, i) => x.ShellIndex === i));
+  assert(node.DataPins.filter(x => x.Direction === "Out").every((x, i) => x.ShellIndex === i));
+  assert(node.DataPins.every(x => x.Direction === "Out" || x.Direction === "In"));
+  assert(node.DataPins.every(x => x.Type.length >= 3));
+
+  if (node.__ref_id === undefined) return;
+  const ref = REF.find(x => x.id === node.__ref_id)!;
+  assert(ref !== undefined);
+
+  if (node.DataPins.filter(x => x.Direction === "Out").length !== ref.ports.filter(p => p.kind === "data-out").length) {
+    console.log(
+      node.Identifier,
+      node.DataPins.filter(x => x.Direction === "Out").length,
+      ref.ports.filter(p => p.kind === "data-out").length
+    );
   }
-  const p = ref[0];
-  assert(p.displayName !== undefined);
-  assert(p.id.length > 0);
-  assert(ref.filter(x => x.id === p.id).length === 1);
-  node.__ref_id = p.id;
+
 });
 
-save("data.json", data);
+// save("data.json", data);
