c672a6ed39d3b0220428ea46c865bc5774208859
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2026-01-01 11:30:10 +0800

    fix

--- utils/node_data/convert.ts
+++ utils/node_data/convert.ts
@@ -33,8 +33,39 @@ data.Nodes.forEach(node => {
   const ref = REF.find(ref => ref.id === node.__ref_id);
   if (!ref) return;
   if (node.FlowPins.length !== ref.ports.filter(x => x.kind.startsWith("flow-")).length) {
-    console.log(node.Identifier);
+    // console.log(node.Identifier);
+    return;
   }
+  let incnt = node.FlowPins.filter(x => x.Direction === "In").length;
+  node.FlowPins.forEach((pin, i) => {
+    const j = pin.Direction === "In" ? i : i - incnt;
+    const refp = ref.ports.filter(x => x.kind.startsWith("flow-"))[i];
+    assert(refp.kind.startsWith("flow-"));
+    assertEq(pin.Direction === "In", refp.kind.endsWith("-in"));
+    pin.ShellIndex ??= j;
+    pin.KernelIndex ??= j;
+    assertEq(pin.ShellIndex, pin.KernelIndex);
+    assertEq(pin.ShellIndex, j);
+    if (!["执行", "完成", "事件"].includes(refp.label)) {
+      console.log(node.Identifier, refp.label);
+    }
+  });
+
+  /** 手动编辑这些:
+Control.General.Branch 是
+Control.General.Branch 否
+Execution.Common_Node.For_Loop 跳出循环
+Execution.Common_Node.For_Loop 循环体
+Execution.Common_Node.For_Loop 循环完成
+Execution.List_Operation.For_Each 跳出循环
+Execution.List_Operation.For_Each 循环体
+Execution.List_Operation.For_Each 循环完成
+Control.General_Client.Branch 是
+Control.General_Client.Branch 否
+Execution.General_Client.For_Loop 跳出循环
+Execution.General_Client.For_Loop 循环体
+Execution.General_Client.For_Loop 循环完成
+  */
 })
 
 
